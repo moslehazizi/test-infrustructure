9c9b76e073dd7ade83a09633ca8e7567504a4ff4
moslehazizi <moslehazizi@gmail.com>
2026-01-07 17:08:30 +0330

    feat: add order by id

--- internal/repository/postgres/mother_service.go
+++ internal/repository/postgres/mother_service.go
@@ -45,7 +45,7 @@ func (m *motherServiceRepository) GetByID(ctx context.Context, id uint64) (*enti
 
 func (m *motherServiceRepository) GetAll(ctx context.Context) ([]*entity.MotherService, error) {
 	var motherServices []*entity.MotherService
-	err := m.db.WithContext(ctx).Find(&motherServices).Error
+	err := m.db.WithContext(ctx).Find(&motherServices).Order("id DESC").Error
 	if err != nil {
 		return nil, fmt.Errorf("failed to get mother service records: %w", err)
 	}
