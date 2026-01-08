6a08a182ba6b767e754facb173419225a81355ee
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2026-01-08 19:06:31 +0330

    feat: add test test scenarios migration

---
+++ migrations/20260107160415_test_scenatios.sql
@@ -0,0 +1,36 @@
+-- migrate:up
+create table if not exists test_scenarios (
+    id bigserial PRIMARY KEY,
+    name varchar(512) NOT NULL,
+
+    test_category_id bigint NOT NULL,
+    CONSTRAINT fk_test_category FOREIGN KEY (test_category_id) REFERENCES test_categories(id) ON DELETE CASCADE ON UPDATE CASCADE,
+
+    mother_service_id bigint NOT NULL,
+    CONSTRAINT fk_mother_service FOREIGN KEY (mother_service_id) REFERENCES mother_services(id) ON DELETE CASCADE ON UPDATE CASCADE,
+
+    -- تعداد سرویس تست قابل تعریف | حداکثر بار
+    max_test_services_count int NULL CHECK (
+        max_test_services_count IS NULL OR max_test_services_count >= 1
+    ),
+
+    -- مدت زمان اجرایی | مانایی در هر مرحله
+    execution_duration int NULL CHECK (
+        execution_duration IS NULL OR execution_duration >= 1
+    ),
+
+    -- معیار افزایش
+    auto_step_increase_rate int NULL CHECK (
+        auto_step_increase_rate IS NULL OR auto_step_increase_rate >= 1
+    ),
+    
+    stopped_at timestamptz default CURRENT_TIMESTAMP,
+    restarted_at timestamptz default CURRENT_TIMESTAMP,
+    started_at timestamptz default CURRENT_TIMESTAMP,
+    created_at timestamptz default CURRENT_TIMESTAMP,
+    updated_at timestamptz default CURRENT_TIMESTAMP,
+    deleted_at timestamptz
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS test_scenarios;
