7a04a4f992b1b9052c9561b9324dc1ca57c0b069
moslehazizi <moslehazizi@gmail.com>
2026-01-10 18:24:28 +0330

    feat: add get by id and get paginated repo mocks

--- internal/repository/mocks/test_scenario.go
+++ internal/repository/mocks/test_scenario.go
@@ -16,3 +16,25 @@ func (m *MockTestScenario) Create(ctx context.Context, testSci *entity.TestScena
 
 	return args.Error(0)
 }
+
+func (m *MockTestScenario) GetByID(ctx context.Context, id uint64) (*entity.TestScenario, error) {
+	args := m.Called(ctx, id)
+
+	var result *entity.TestScenario
+	if args.Get(0) != nil {
+		result = args.Get(0).(*entity.TestScenario)
+	}
+
+	return result, args.Error(1)
+}
+
+func (m *MockTestScenario) GetPaginated(ctx context.Context, pagRequest entity.TestScenarioPaginationRequest) ([]*entity.TestScenario, error) {
+	args := m.Called(ctx, pagRequest)
+
+	var result []*entity.TestScenario
+	if args.Get(0) != nil {
+		result = args.Get(0).([]*entity.TestScenario)
+	}
+
+	return result, args.Error(1)
+}
