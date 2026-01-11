716cac46748a75f01db33e6d734bc229c08b0bee
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2026-01-11 19:57:50 +0330

    fix: add deleted_at col to mother service

--- migrations/20260107123206_mother_services.sql
+++ migrations/20260107123206_mother_services.sql
@@ -34,6 +34,7 @@ create table if not exists mother_services (
     
     created_at timestamptz default CURRENT_TIMESTAMP,
     updated_at timestamptz default CURRENT_TIMESTAMP,
+    deleted_at timestamptz,
 
     -- CHECK constraint that validates response delay configuration
     -- Ensures one of three valid delay scenarios:
