eaca323e66183cf7d0316f927f80e8ec0433c5f2
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2026-01-11 15:03:32 +0330

    feat: send kafka event on mother service creation

--- internal/repository/mother_service.go
+++ internal/repository/mother_service.go
@@ -9,4 +9,7 @@ type MotherServiceRepository interface {
 	Create(ctx context.Context, motherService *entity.MotherService) error
 	GetByID(ctx context.Context, id uint64) (*entity.MotherService, error)
 	GetPaginated(ctx context.Context, paginationRequest entity.PaginationRequest) ([]*entity.MotherService, error)
+	Begin() MotherServiceRepository
+	Commit() error
+	Rollback() error
 }
