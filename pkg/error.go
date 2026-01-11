a5d81cf55b357143c514643ae590dc857b62c56b
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2026-01-11 22:13:07 +0330

    refactor: update errors and define a unified error handler to handle app http errors based on error type

--- pkg/error.go
+++ pkg/error.go
@@ -2,10 +2,128 @@ package pkg
 
 import (
 	"errors"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
 )
+
+type HTTPError struct {
+	status int
+	msg    string
+}
+
+func (e *HTTPError) AsFiber(ctx *fiber.Ctx) error {
+	return ctx.Status(e.status).JSON(&fiber.Map{
+		"error": e.msg,
+	})
+}
+
+func ToHTTPError(err error) *HTTPError {
+	var status int
+	var msg string
+
+	switch {
+	case errors.Is(err, ErrInternalServerError),
+		errors.Is(err, ErrFailedToConsumeData),
+		errors.Is(err, ErrFailedToUnmarshalEventData),
+		errors.Is(err, ErrFailedToLoadConfig),
+		errors.Is(err, ErrFailedToGetMotherService),
+		errors.Is(err, ErrFailedToGetMotherServices),
+		errors.Is(err, ErrNegativePageOrPerPageNotAllowed),
+		errors.Is(err, ErrFailedToGetTestScenario),
+		errors.Is(err, ErrFailedToGetTestScenarios),
+		errors.Is(err, ErrFailedToCreateTestScenario),
+		errors.Is(err, ErrFailedToGetTestCategory),
+		errors.Is(err, ErrFailedToGetTestCategoryFromRepository),
+		errors.Is(err, ErrFailedToGetTestCategoriesFromRepository),
+		errors.Is(err, ErrFailedToCreateMotherService),
+		errors.Is(err, ErrFailedToSendProvisioningEvent),
+		errors.Is(err, ErrFailedToSendEventData):
+		status = http.StatusInternalServerError
+		msg = InternalServerErrorMessage
+	case errors.Is(err, ErrMotherServiceNotFound):
+		status = http.StatusNotFound
+		msg = MotherServiceNotFound
+	case errors.Is(err, ErrTestScenarioNotFound):
+		status = http.StatusNotFound
+		msg = TestScenarioNotFound
+	case errors.Is(err, ErrTestCategoryNotFound):
+		status = http.StatusNotFound
+		msg = TestCategoryNotFound
+	case errors.Is(err, ErrMotherServiceAlreadyExist):
+		status = http.StatusConflict
+		msg = MotherServiceAlreadyExist
+	case errors.Is(err, ErrBadRequest):
+		status = http.StatusBadRequest
+		msg = InvalidReqBody
+	case errors.Is(err, ErrInvalidIDInParams):
+		status = http.StatusBadRequest
+		msg = InvalidIDInParams
+
+	case errors.Is(err, ErrInvalidMotherServiceName):
+		status = http.StatusUnprocessableEntity
+		msg = InvalidMotherServiceName
+	case errors.Is(err, ErrInvalidExceptionRate):
+		status = http.StatusUnprocessableEntity
+		msg = InvalidExceptionRate
+	case errors.Is(err, ErrInvalidResponseDelayRate):
+		status = http.StatusUnprocessableEntity
+		msg = InvalidResponseDelayRate
+	case errors.Is(err, ErrInvalidDelayConfiguration):
+		status = http.StatusUnprocessableEntity
+		msg = InvalidDelayConfiguration
+	case errors.Is(err, ErrInvalidRandomDelayRange):
+		status = http.StatusUnprocessableEntity
+		msg = InvalidRandomDelayRange
+	case errors.Is(err, ErrInvalidDatabaseName):
+		status = http.StatusUnprocessableEntity
+		msg = InvalidDatabaseName
+	case errors.Is(err, ErrInvalidDatabaseTableName):
+		status = http.StatusUnprocessableEntity
+		msg = InvalidDatabaseTableName
+	case errors.Is(err, ErrExecutionDurationLessThanOne):
+		status = http.StatusUnprocessableEntity
+		msg = ExecutionDurationLessThanOne
+	case errors.Is(err, ErrMaxTestServiceCountLessThanOne):
+		status = http.StatusUnprocessableEntity
+		msg = MaxTestServiceCountLessThanOne
+	case errors.Is(err, ErrAutoStepChangeRateLessThanOne):
+		status = http.StatusUnprocessableEntity
+		msg = AutoStepChangeRateLessThanOne
+	case errors.Is(err, ErrMaxTestServiceCountNotSet):
+		status = http.StatusUnprocessableEntity
+		msg = MaxTestServiceCountNotSet
+	case errors.Is(err, ErrNoNeedMaxTestServiceCount):
+		status = http.StatusUnprocessableEntity
+		msg = NoNeedMaxTestServiceCount
+	case errors.Is(err, ErrExecutionDurationNotSet):
+		status = http.StatusUnprocessableEntity
+		msg = ExecutionDurationNotSet
+	case errors.Is(err, ErrNoNeedExecutionDuration):
+		status = http.StatusUnprocessableEntity
+		msg = NoNeedExecutionDuration
+	case errors.Is(err, ErrAutoStepChangeNotSet):
+		status = http.StatusUnprocessableEntity
+		msg = AutoStepChangeNotSet
+	case errors.Is(err, ErrNoNeedAutoStepChange):
+		status = http.StatusUnprocessableEntity
+		msg = NoNeedAutoStepChange
+	case errors.Is(err, ErrPageNotFound):
+		status = http.StatusNotFound
+		msg = PageNotFound
+
+	default:
+		status = http.StatusInternalServerError
+		msg = InternalServerErrorMessage
+	}
+
+	return &HTTPError{status, msg}
+}
 
 var (
 	ErrInternalServerError             = errors.New("internal server error")
+	ErrBadRequest                      = errors.New("bad request")
+	ErrPageNotFound                    = errors.New("404 page not found")
 	ErrFailedToSendEventData           = errors.New("failed to send event data")
 	ErrFailedToConsumeData             = errors.New("failed to consume data")
 	ErrFailedToUnmarshalEventData      = errors.New("failed to unmarshal factorial event")
@@ -22,17 +140,16 @@ var (
 	ErrFailedToCreateTestScenario      = errors.New("failed to create test scenario")
 	ErrTestCategoryNotFound            = errors.New("test category not found")
 	ErrFailedToGetTestCategory         = errors.New("failed to get test category record")
+	ErrInvalidIDInParams               = errors.New("invalid id in params")
 
 	// Validation errors.
-	ErrInvalidName                             = errors.New("mother service name is required")
+	ErrInvalidMotherServiceName                = errors.New("mother service name is required")
 	ErrInvalidExceptionRate                    = errors.New("exception rate must be between 0 and 100")
 	ErrInvalidResponseDelayRate                = errors.New("response delay rate must be between 0 and 100")
 	ErrInvalidDelayConfiguration               = errors.New("invalid delay configuration: must be either no delay, fixed delay, or random delay")
 	ErrInvalidRandomDelayRange                 = errors.New("random delay min must be less than max")
 	ErrInvalidDatabaseName                     = errors.New("database name is required")
 	ErrInvalidDatabaseTableName                = errors.New("database table name is required")
-	ErrInvalidKafkaLivefeedTopic               = errors.New("kafka livefeed topic is required")
-	ErrInvalidKafkaFactorialTopic              = errors.New("kafka factorial topic is required")
 	ErrFailedToGetTestCategoriesFromRepository = errors.New("failed to get test categories from repository")
 	ErrFailedToGetTestCategoryFromRepository   = errors.New("failed to get test category from repository")
 	ErrExecutionDurationLessThanOne            = errors.New("execution duration should be more than one")
