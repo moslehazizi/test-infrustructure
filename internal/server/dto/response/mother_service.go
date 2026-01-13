b27fdc4e8ed78473e6bb5ab5d16d89457e68c18c
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2026-01-13 09:49:44 +0330

    fix: change status data type of mother service response

--- internal/server/dto/response/mother_service.go
+++ internal/server/dto/response/mother_service.go
@@ -1,21 +1,22 @@
 package response
 
 import (
+	"control-panel-service/internal/domain/entity"
 	"time"
 )
 
 type MotherService struct {
-	ID                       uint64    `json:"id"`
-	CreatedAt                time.Time `json:"created_at"`
-	UpdatedAt                time.Time `json:"updated_at"`
-	Name                     string    `json:"name"`
-	ExceptionRate            int       `json:"exception_rate"`
-	ResponseDelayRate        int       `json:"response_delay_rate"`
-	ResponseDelayDuration    *int      `json:"response_delay_duration"`
-	RandomResponseDelayMin   *int      `json:"random_response_delay_min"`
-	RandomResponseDelayMax   *int      `json:"random_response_delay_max"`
-	Status                   string    `json:"status"`
-	ServiceDeploymentAddress *string   `json:"service_deployment_address"`
-	DatabaseName             string    `json:"database_name"`
-	DatabaseTableName        string    `json:"database_table_name"`
+	ID                       uint64                     `json:"id"`
+	CreatedAt                time.Time                  `json:"created_at"`
+	UpdatedAt                time.Time                  `json:"updated_at"`
+	Name                     string                     `json:"name"`
+	ExceptionRate            int                        `json:"exception_rate"`
+	ResponseDelayRate        int                        `json:"response_delay_rate"`
+	ResponseDelayDuration    *int                       `json:"response_delay_duration"`
+	RandomResponseDelayMin   *int                       `json:"random_response_delay_min"`
+	RandomResponseDelayMax   *int                       `json:"random_response_delay_max"`
+	Status                   entity.MotherServiceStatus `json:"status"`
+	ServiceDeploymentAddress *string                    `json:"service_deployment_address"`
+	DatabaseName             string                     `json:"database_name"`
+	DatabaseTableName        string                     `json:"database_table_name"`
 }
