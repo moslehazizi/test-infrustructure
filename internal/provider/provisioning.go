b8b24b074f1590b3a4a7947479d56a68dec71f96
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2026-02-08 13:47:48 +0330

    deal on provisioning service

--- internal/provider/provisioning.go
+++ internal/provider/provisioning.go
@@ -6,24 +6,9 @@ import (
 )
 
 type ProvisioningService interface {
-	ProvisionTestService(ctx context.Context, testServiceConfig *entity.TestServiceConfig, count int) error
-	// TODO: it should be aligned with Nikola
-	DeprovisionTestService(ctx context.Context) error
-}
+	ProvisionTestService(ctx context.Context, testScenario *entity.TestScenario, replica int32) error
+	DeprovisionTestService(ctx context.Context, testScenario *entity.TestScenario, replica int32) error
 
-func NewProvisioningService() ProvisioningService {
-	return &k8sProvisioningService{}
-}
-
-type k8sProvisioningService struct {
-}
-
-func (p *k8sProvisioningService) ProvisionTestService(ctx context.Context, testServiceConfig *entity.TestServiceConfig, count int) error {
-	// TODO: use Nikola's work
-	return nil
-}
-
-func (p *k8sProvisioningService) DeprovisionTestService(ctx context.Context) error {
-	// TODO: use Nikola's work
-	return nil
+	ProvisionMotherService(ctx context.Context, motherService *entity.MotherService) error
+	DeprovisionMotherService(ctx context.Context, motherService *entity.MotherService) error
 }
