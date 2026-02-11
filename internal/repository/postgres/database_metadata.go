50afbbc0cc9c166b8e8263d7329d38a205a80f5d
moslehazizi <moslehazizi@gmail.com>
2026-02-11 21:54:22 +0330

    fix: fix repository layer, add connection

--- internal/repository/postgres/database_metadata.go
+++ internal/repository/postgres/database_metadata.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"control-panel-service/config"
 	"control-panel-service/internal/repository"
 	"control-panel-service/pkg/database"
 	"control-panel-service/pkg/database/postgres"
@@ -12,14 +13,16 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 )
 
-func NewDatabaseMetadataRepository(db database.Database) repository.DatabaseMetadata {
+func NewDatabaseMetadataRepository(db database.Database, cfg *config.Config) repository.DatabaseMetadata {
 	return &databaseMetadata{
 		db,
+		cfg,
 	}
 }
 
 type databaseMetadata struct {
-	db database.Database
+	db  database.Database
+	cfg *config.Config
 }
 
 func (repo *databaseMetadata) GetAll(ctx context.Context) ([]string, error) {
@@ -61,34 +64,49 @@ func (repo *databaseMetadata) GetAll(ctx context.Context) ([]string, error) {
 
 func (repo *databaseMetadata) GetTablesByDBName(ctx context.Context, dbName string) ([]string, error) {
 	tracer := otel.Tracer("database-metadata-repository")
-	_, span := tracer.Start(ctx, "get_tables_by_db_name")
+	ctx, span := tracer.Start(ctx, "get_tables_by_db_name")
 	defer span.End()
 
 	requestID := logger.GetRequestID(ctx)
 	span.SetAttributes(
 		attribute.String("request_id", requestID),
 		attribute.String("database.name", dbName),
-		attribute.String("database.operation", "select"),
+		attribute.String("database.operation", "select_tables"),
 	)
+
+	db, err := postgres.New(&postgres.DatabaseConfig{
+		Host:               repo.cfg.Postgres.Host,
+		Port:               repo.cfg.Postgres.Port,
+		User:               repo.cfg.Postgres.User,
+		Password:           repo.cfg.Postgres.Password,
+		Database:           dbName,
+		MaxOpenConnections: repo.cfg.Postgres.MaxOpenConnections,
+		LogLevel:           postgres.Silent,
+	})
+	if err != nil {
+		span.RecordError(err)
+		span.SetAttributes(attribute.String("error.type", "connection_error"))
+
+		return nil, fmt.Errorf("failed to connect to database %s: %w", dbName, err)
+	}
+	defer db.Close()
 
 	var tables []string
 
-	query := fmt.Sprintf(`
-		SELECT table_name
-		FROM information_schema.tables
-		WHERE table_catalog = '%s' AND table_schema = 'public'
-		ORDER BY table_name
-	`, dbName)
+	query := `
+		SELECT tablename
+		FROM pg_catalog.pg_tables
+		WHERE schemaname = 'public'
+		ORDER BY tablename;
+	`
 
-	err := postgres.QueryBuilder(ctx, repo.db).
+	err = postgres.QueryBuilder(ctx, db).
 		Raw(query).
 		Scan(&tables).Error
 
 	if err != nil {
-		span.SetAttributes(
-			attribute.String("error.type", "database_error"),
-			attribute.String("error.message", err.Error()),
-		)
+		span.RecordError(err)
+		span.SetAttributes(attribute.String("error.type", "query_error"))
 
 		return nil, fmt.Errorf("failed to load tables for database %s: %w", dbName, err)
 	}
