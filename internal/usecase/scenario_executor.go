fbad5b7fb129ee5b98dbe5487d30affb245cf66f
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2026-04-04 12:54:34 +0330

    refactor: create new file to store scenario executor implementations

---
+++ internal/usecase/scenario_executor.go
@@ -0,0 +1,183 @@
+package usecase
+
+import (
+	"context"
+	"control-panel-service/internal/domain/entity"
+	"control-panel-service/internal/provider/dto/request"
+	"control-panel-service/internal/repository"
+	"control-panel-service/internal/usecase/interfaces"
+	"control-panel-service/pkg"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+const (
+	RunOnceDelay = time.Millisecond * 100
+)
+
+var (
+	healthyCheckSleep         = time.Second
+	readyForTestingCheckSleep = time.Second
+)
+
+type scenarioExecutor struct {
+	scenario                 *entity.TestScenario
+	executionID              uuid.UUID
+	agents                   []interfaces.TestAgentController
+	allAgentsHealthy         bool
+	allAgentsReadyForTesting bool
+	running                  bool
+	once                     sync.Once
+	scenarioRepo             repository.TestScenarioRepository
+}
+
+func (sc *scenarioExecutor) IsRunning() bool {
+	return sc.running
+}
+
+func (sc *scenarioExecutor) SetRunning(status bool) {
+	sc.running = status
+}
+
+func (sc *scenarioExecutor) SetExecutionID(execID uuid.UUID) {
+	sc.executionID = execID
+}
+
+func (sc *scenarioExecutor) AddAgent(agent interfaces.TestAgentController) {
+	sc.agents = append(sc.agents, agent)
+}
+
+func (sc *scenarioExecutor) GetAgents() []interfaces.TestAgentController {
+	return sc.agents
+}
+
+func (sc *scenarioExecutor) AllAgentsAreHealthy() bool {
+	return sc.allAgentsHealthy
+}
+
+func (sc *scenarioExecutor) Run(ctx context.Context) error {
+	sc.once.Do(func() {
+		zap.L().Info("scenarioExecutor.RunOnce Called")
+
+	start:
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			for !sc.running {
+				time.Sleep(RunOnceDelay)
+
+				zap.L().Debug("scenarioExecutor.RunOnce waiting to be run")
+			}
+			zap.L().Info("scenarioExecutor.RunOnce Running")
+
+			// Make sure all agents are healthy.
+			sc.awaitAgentsToBeHealthy()
+
+			// NOW: all agents are healthy.
+			// we can send scheduled commands.
+			// we need a loop based on len of steps:
+			_ = sc.executeScenarioSteps(ctx)
+
+			goto start
+		}
+	})
+
+	return nil
+}
+
+func (sc *scenarioExecutor) awaitAgentsToBeHealthy() {
+	for {
+		allHealthy := true
+		for _, agent := range sc.agents {
+			if !agent.Healthy() {
+				allHealthy = false
+
+				break
+			}
+		}
+
+		sc.allAgentsHealthy = allHealthy
+
+		if allHealthy {
+			break
+		}
+
+		time.Sleep(healthyCheckSleep)
+	}
+}
+
+func (sc *scenarioExecutor) awaitAgentsToBeReadyToStartTesting() {
+	for {
+		allReady := true
+		for _, agent := range sc.agents {
+			if !agent.ReadyForTesting() {
+				allReady = false
+
+				break
+			}
+		}
+
+		sc.allAgentsReadyForTesting = allReady
+
+		if allReady {
+			break
+		}
+
+		time.Sleep(readyForTestingCheckSleep)
+	}
+}
+
+func (sc *scenarioExecutor) executeScenarioSteps(ctx context.Context) error {
+	// sc.scenario.IncreaseAgentNumber
+	// sc.scenario.ExecNumMultiAgent
+	// sc.scenario.TestServiceConfig.IncreaseFixedInput
+	// sc.scenario.TestServiceConfig.ExecNumMultiFixedInput
+
+	for i := int64(1); i <= sc.scenario.NumSteps; i++ {
+		if !sc.running {
+			zap.L().Info("scenario executor is not running; exiting scenarioExecutor.Run")
+
+			return pkg.ErrScenarioIsNotRunning
+		}
+		req := request.NewRunRequestFromTestServiceConfig(
+			int(i),
+			sc.executionID,
+			sc.scenario.TestServiceConfig,
+		)
+
+		// send command to all agents.
+		wg := sync.WaitGroup{}
+		for _, agent := range sc.agents {
+			wg.Add(1)
+			//nolint
+			go func() {
+				defer wg.Done()
+				_ = agent.StartTesting(context.Background(), *req)
+			}()
+		}
+
+		wg.Wait()
+
+		if i < sc.scenario.NumSteps {
+			// wait for all agents to be ready to execute next step.
+			sc.awaitAgentsToBeReadyToStartTesting()
+		} else {
+			// set status ScenarioStatusPending
+			err := sc.scenarioRepo.SetStatus(ctx, sc.scenario.ID, entity.ScenarioStatusPending, true)
+			if err != nil {
+				zap.L().Error("failed to update scenario executor status", zap.Error(err))
+
+				return fmt.Errorf("%w: %w", pkg.ErrFailedToSetScenarioStatus, err)
+			}
+			// set running false , to privent run again and again
+			sc.SetRunning(false)
+		}
+	}
+
+	return nil
+}
