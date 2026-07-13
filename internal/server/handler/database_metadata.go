b7e9526023541ac70a604f9793e44569986d9d1e
shaghasemi <shaghasemi@gmail.com>
2026-07-13 09:20:12 +0330

    refactor: database handlers done - ongoing

--- internal/server/handler/database_metadata.go
+++ internal/server/handler/database_metadata.go
@@ -1,27 +1,28 @@
 package handler
 
-// import (
-// 	"control-panel-service/internal/server/dto/request"
-// 	"control-panel-service/internal/server/dto/response"
-// 	"control-panel-service/pkg"
-// 	"control-panel-service/pkg/logger"
-// 	"net/http"
-// 	"strings"
+import (
+	"control-panel-service/internal/server/dto/request"
+	"control-panel-service/internal/server/dto/response"
+	"control-panel-service/pkg"
+	"control-panel-service/pkg/logger"
+	"control-panel-service/pkg/responsewriter"
+	"encoding/json"
+	"net/http"
+	"strings"
 
-// 	"github.com/gofiber/fiber/v2"
-// 	"go.opentelemetry.io/otel"
-// 	"go.opentelemetry.io/otel/attribute"
-// )
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+)
 
-// func NewDatabaseMetadataHandler(databaseMetaDataService DatabaseMetadata) *DatabaseMetadataHandler {
-// 	return &DatabaseMetadataHandler{
-// 		databaseMetaDataService: databaseMetaDataService,
-// 	}
-// }
+func NewDatabaseMetadataHandler(databaseMetaDataService DatabaseMetadata) *DatabaseMetadataHandler {
+	return &DatabaseMetadataHandler{
+		databaseMetaDataService: databaseMetaDataService,
+	}
+}
 
-// type DatabaseMetadataHandler struct {
-// 	databaseMetaDataService DatabaseMetadata
-// }
+type DatabaseMetadataHandler struct {
+	databaseMetaDataService DatabaseMetadata
+}
 
 // // GetAll godoc
 // //
@@ -33,26 +34,26 @@ package handler
 // //	@Success		200	{object}	response.DatabaseMetadataDatabasesResponse
 // //	@Failure		500	{object}	response.ErrorResponse
 // //	@Router			/api/v1/databases [get]
-// func (handler *DatabaseMetadataHandler) GetAll() fiber.Handler {
-// 	return func(ctx *fiber.Ctx) error {
-// 		tracer := otel.Tracer("database-metadata-handler")
-// 		traceCtx, span := tracer.Start(ctx.Context(), "get-databases-handler")
-// 		defer span.End()
+func (handler *DatabaseMetadataHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+	tracer := otel.Tracer("database-metadata-handler")
+	traceCtx, span := tracer.Start(r.Context(), "get-databases-handler")
+	defer span.End()
 
-// 		requestID := logger.GetRequestID(ctx.Context())
-// 		span.SetAttributes(attribute.String("request_id", requestID))
+	requestID := logger.GetRequestID(r.Context())
+	span.SetAttributes(attribute.String("request_id", requestID))
 
-// 		result, err := handler.databaseMetaDataService.GetAll(traceCtx)
-// 		if err != nil {
-// 			span.SetAttributes(attribute.String("error.type", "usecase_error"))
-// 			return pkg.ToHTTPError(err).AsFiber(ctx)
-// 		}
+	result, err := handler.databaseMetaDataService.GetAll(traceCtx)
+	if err != nil {
+		span.SetAttributes(attribute.String("error.type", "usecase_error"))
+		httpError := pkg.ToHTTPError(err)
+		httpError.WriteError(w)
+		return
+	}
 
-// 		return ctx.Status(http.StatusOK).JSON(&response.DatabaseMetadataDatabasesResponse{
-// 			Data: result,
-// 		})
-// 	}
-// }
+	responsewriter.WriteJSON(w, http.StatusOK, response.DatabaseMetadataDatabasesResponse{
+		Data: result,
+	})
+}
 
 // // GetTablesByDBNamePost godoc
 // //
@@ -66,45 +67,50 @@ package handler
 // //	@Failure		400		{object}	response.ErrorResponse
 // //	@Failure		500		{object}	response.ErrorResponse
 // //	@Router			/api/v1/databases/tables [post]
-// func (handler *DatabaseMetadataHandler) GetTablesByDBNamePost() fiber.Handler {
-// 	return func(ctx *fiber.Ctx) error {
-// 		tracer := otel.Tracer("database-metadata-handler")
-// 		traceCtx, span := tracer.Start(ctx.Context(), "get-tables-by-database-handler")
-// 		defer span.End()
+func (handler *DatabaseMetadataHandler) GetTablesByDBNamePost(w http.ResponseWriter, r *http.Request) {
+	tracer := otel.Tracer("database-metadata-handler")
+	traceCtx, span := tracer.Start(r.Context(), "get-tables-by-database-handler")
+	defer span.End()
 
-// 		requestID := logger.GetRequestID(ctx.Context())
-// 		span.SetAttributes(attribute.String("request_id", requestID))
+	requestID := logger.GetRequestID(r.Context())
+	span.SetAttributes(attribute.String("request_id", requestID))
 
-// 		req := new(request.GetTablesRequest)
-// 		if err := ctx.BodyParser(req); err != nil {
-// 			span.SetAttributes(attribute.String("error.type", "bad_request"))
-// 			return pkg.ToHTTPError(pkg.ErrBadRequest).AsFiber(ctx)
-// 		}
+	req := new(request.GetTablesRequest)
+	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+		span.SetAttributes(attribute.String("error.type", "bad_request"))
+		httpError := pkg.ToHTTPError(pkg.ErrBadRequest)
+		httpError.WriteError(w)
+		return
+	}
 
-// 		dbName := strings.TrimSpace(req.DatabaseName)
-// 		if dbName == "" {
-// 			span.SetAttributes(attribute.String("error.type", "invalid_db_name"))
-// 			return pkg.ToHTTPError(pkg.ErrBadRequest).AsFiber(ctx)
-// 		}
+	dbName := strings.TrimSpace(req.DatabaseName)
+	if dbName == "" {
+		span.SetAttributes(attribute.String("error.type", "invalid_db_name"))
+		httpError := pkg.ToHTTPError(pkg.ErrBadRequest)
+		httpError.WriteError(w)
+		return
+	}
 
-// 		span.SetAttributes(attribute.String("database.name", dbName))
+	span.SetAttributes(attribute.String("database.name", dbName))
 
-// 		result, err := handler.databaseMetaDataService.GetTablesByDBName(traceCtx, dbName)
-// 		if err != nil {
-// 			span.SetAttributes(attribute.String("error.type", "usecase_error"), attribute.String("error.message", err.Error()))
-// 			return pkg.ToHTTPError(err).AsFiber(ctx)
-// 		}
+	result, err := handler.databaseMetaDataService.GetTablesByDBName(traceCtx, dbName)
+	if err != nil {
+		span.SetAttributes(attribute.String("error.type", "usecase_error"), attribute.String("error.message", err.Error()))
+		httpError := pkg.ToHTTPError(err)
+		httpError.WriteError(w)
+		return
+	}
 
-// 		if result == nil {
-// 			return ctx.Status(http.StatusOK).JSON(&response.TablesByType{
-// 				MotherTables: []string{},
-// 				TestTables:   []string{},
-// 			})
-// 		}
+	if result == nil {
+		responsewriter.WriteJSON(w, http.StatusOK, response.TablesByType{
+			MotherTables: []string{},
+			TestTables:   []string{},
+		})
+	}
 
-// 		return ctx.Status(http.StatusOK).JSON(&response.TablesByType{
-// 			MotherTables: result.MotherTables,
-// 			TestTables:   result.TestTables,
-// 		})
-// 	}
-// }
+	responsewriter.WriteJSON(w, http.StatusOK, response.TablesByType{
+		MotherTables: result.MotherTables,
+		TestTables:   result.TestTables,
+	})
+
+}
